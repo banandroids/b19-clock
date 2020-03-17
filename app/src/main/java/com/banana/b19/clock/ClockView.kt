3bf232f7387a683583c2577f6f4d81d65a06a079
kuelye <kuelye@gmail.com>
2020-03-17 22:22:36 +0300

    II.19

--- app/src/main/java/com/banana/b19/clock/ClockView.kt
+++ app/src/main/java/com/banana/b19/clock/ClockView.kt
@@ -2,18 +2,22 @@ package com.banana.b19.clock
 
 import android.animation.ValueAnimator
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.GestureDetector
 import android.view.MotionEvent
-import android.view.MotionEvent.ACTION_DOWN
-import android.view.MotionEvent.ACTION_MOVE
+import android.view.ScaleGestureDetector
 import android.view.View
-import android.view.animation.BounceInterpolator
 import android.view.animation.OvershootInterpolator
 import java.util.*
 import kotlin.math.min
+
+const val SP_NAME = "test"
+const val SP_SCALE = "SCALE"
 
 class ClockView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -26,6 +30,12 @@ class ClockView @JvmOverloads constructor(
     val animator: ValueAnimator = ValueAnimator()
 
     var bgColor = Color.BLUE
+
+    private lateinit var scaleGestureDetector: ScaleGestureDetector
+    private lateinit var gestureDetector: GestureDetector
+    private var scale = 1f
+    private var dX = 0f
+    private var dY = 0f
 
     init {
         calendar.timeZone = TimeZone.getTimeZone("GMT+3")
@@ -45,13 +55,15 @@ class ClockView @JvmOverloads constructor(
             }
         }
         timer.schedule(timerTask, 1000, 1000)
+
+        initializeGestures()
     }
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawColor(bgColor) // закрашиваем полностью холст красным
-        val cx = (width / 2).toFloat() // x-координата центра
-        val cy = (height / 2).toFloat() // y-координата центра
-        val r = min(cx, cy) - 10 // радиус
+        val cx = (width / 2).toFloat() + dX // x-координата центра
+        val cy = (height / 2).toFloat() + dY // y-координата центра
+        val r = (min(width / 2, height / 2) - 10) * scale // радиус
         paint.color = Color.WHITE
         canvas.drawCircle(cx, cy, r, paint) // рисуем контур часов
 
@@ -66,12 +78,64 @@ class ClockView @JvmOverloads constructor(
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == ACTION_DOWN || event.action == ACTION_MOVE) {
-            bgColor = Color.rgb(event.x.toInt(), event.y.toInt(), 0)
-            invalidate()
-            return true
-        }
-        return super.onTouchEvent(event)
+        scaleGestureDetector.onTouchEvent(event)
+        gestureDetector.onTouchEvent(event)
+        return true
+    }
+
+    /* метод вызывается когда вьюшка добавляется в приложение */
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        load() // загружаем состояние
+    }
+
+    fun onPause() {
+        save() // когда активность паузится - сохраняем состояние
+    }
+
+    /* метод настраивает жесты */
+    private fun initializeGestures() {
+        scaleGestureDetector = ScaleGestureDetector(context, object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+            override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
+                return true // true - чтобы и дальше приходили события
+            }
+
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                scale *= detector.scaleFactor // обновляем скейл
+                invalidate()
+                return true // true - чтобы и дальше приходили события
+            }
+        })
+
+        gestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onScroll(
+                e1: MotionEvent?,
+                e2: MotionEvent?,
+                distanceX: Float,
+                distanceY: Float
+            ): Boolean {
+                dX -= distanceX
+                dY -= distanceY
+                invalidate()
+                return true // true - чтобы и дальше приходили события
+            }
+
+            override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+                bgColor = Color.rgb(e.x.toInt(), e.y.toInt(), 0)
+                invalidate()
+                return true
+            }
+
+            override fun onLongPress(e: MotionEvent) {
+                dX = e.x - width / 2
+                dY = e.y - height / 2
+                invalidate()
+            }
+
+            override fun onDoubleTap(e: MotionEvent?): Boolean {
+                return super.onDoubleTap(e)
+            }
+        })
     }
 
     private fun animateSeconds(newSeconds: Int) {
@@ -85,9 +149,9 @@ class ClockView @JvmOverloads constructor(
     }
 
     private fun drawHand(canvas: Canvas, handType: HandType, angle: Float) {
-        val cx = (width / 2).toFloat() // x-координата центра
-        val cy = (height / 2).toFloat() // y-координата центра
-        val r = min(cx, cy) - 10 // радиус
+        val cx = (width / 2).toFloat() + dX // x-координата центра
+        val cy = (height / 2).toFloat() + dY // y-координата центра
+        val r = (min(width / 2, height / 2) - 10) * scale // радиус
         val l = r * handType.length
 
         // рисуем стрелку
@@ -96,6 +160,25 @@ class ClockView @JvmOverloads constructor(
         paint.color = handType.color
         canvas.drawLine(cx, cy, cx, cy - l, paint) // рисуем линию
         canvas.restore()
+    }
+
+    /* метод сохраняет состояние часов */
+    private fun save() {
+        // достаём файлик с сохранёнками (в SP_NAME - название файлика)
+        val sp = context.getSharedPreferences(SP_NAME, MODE_PRIVATE)
+        sp.edit() // открываем файлик
+            .putFloat(SP_SCALE, scale) // кладём туда scale в ящик с названием SP_SCALE
+            // TODO сохранять dX и dY ещё
+            .apply() // сохраняем изменения
+    }
+
+    /* метод загружает состояние часов */
+    private fun load() {
+        // достаём файлик с сохранёнками (в SP_NAME - название файлика)
+        val sp = context.getSharedPreferences(SP_NAME, MODE_PRIVATE)
+        scale = sp.getFloat(SP_SCALE, 1f) // загружаем скейл
+        // TODO загружать dX и dY ещё
+        invalidate() // запрашиваем перерисовку
     }
 
     private enum class HandType(
--- app/src/main/java/com/banana/b19/clock/MainActivity.kt
+++ app/src/main/java/com/banana/b19/clock/MainActivity.kt
@@ -2,6 +2,7 @@ package com.banana.b19.clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onPause() {
+        super.onPause()
+        clockView.onPause()
+    }
+
 }
